65a534321db3bf51fb223cf12eee6308fbaa7612
Andrew Wong <andrewsulliwong@gmail.com>
2017-09-30 15:05:05 -0400

    msg work

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var port = process.env.PORT || 8080;
+
 
 io.on('connection', function(client){
-  console.log("Client connected...");
+  console.log('Client Connection!');
+  //client.emit('messages', { hello: 'world' });
+  client.on('messages', function(data){
+    console.log(data);
+    io.emit("messages", data);
+  });
+
+  client.on('disconnect', function(){
+    console.log("Client disconnected")
+  });
+
 });
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(8080);
+server.listen(port);
